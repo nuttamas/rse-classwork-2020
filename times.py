9c434702fc03ff1d583f7c6ce8df1965d4e2c72a
NuttamasT <Nuttamas.tubsrinuan.20@ucl.ac.uk>
2020-11-05 20:02:15 +0000

    re-update times.py

--- times.py
+++ times.py
@@ -1,13 +1,12 @@
 import datetime
 
-
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
+                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
+                for i in range(number_of_intervals)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 
@@ -23,4 +22,4 @@ def compute_overlap_time(range1, range2):
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    print(compute_overlap_time(large, short))
+    print(compute_overlap_time(large, short)) 
